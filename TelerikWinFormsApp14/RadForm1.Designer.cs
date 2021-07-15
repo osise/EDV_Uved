98c54b2501bf6d4eca934193bc62b4dc530fb611
S_O <spacecore66@gmail.com>
2021-07-15 13:43:51 +0500

    Добавьте файлы проекта.

---
+++ TelerikWinFormsApp14/RadForm1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace TelerikWinFormsApp14
+{
+    partial class RadForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RadForm1));
+            this.fluentTheme1 = new Telerik.WinControls.Themes.FluentTheme();
+            this.radButton2 = new Telerik.WinControls.UI.RadButton();
+            this.radRichTextEditor1 = new Telerik.WinControls.UI.RadRichTextEditor();
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.radButton3 = new Telerik.WinControls.UI.RadButton();
+            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
+            this.radButton4 = new Telerik.WinControls.UI.RadButton();
+            this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
+            this.radButton5 = new Telerik.WinControls.UI.RadButton();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radRichTextEditor1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radButton2
+            // 
+            this.radButton2.BackColor = System.Drawing.Color.LightBlue;
+            this.radButton2.Font = new System.Drawing.Font("Segoe UI", 14.25F);
+            this.radButton2.Location = new System.Drawing.Point(539, 8);
+            this.radButton2.Name = "radButton2";
+            this.radButton2.Size = new System.Drawing.Size(147, 45);
+            this.radButton2.TabIndex = 11;
+            this.radButton2.Text = "Создать уведомления";
+            this.radButton2.ThemeName = "Fluent";
+            this.radButton2.Click += new System.EventHandler(this.radButton2_Click);
+            // 
+            // radRichTextEditor1
+            // 
+            this.radRichTextEditor1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(189)))), ((int)(((byte)(232)))));
+            this.radRichTextEditor1.Location = new System.Drawing.Point(8, 8);
+            this.radRichTextEditor1.Name = "radRichTextEditor1";
+            this.radRichTextEditor1.SelectionFill = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(78)))), ((int)(((byte)(158)))), ((int)(((byte)(255)))));
+            this.radRichTextEditor1.SelectionStroke = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(236)))), ((int)(((byte)(248)))));
+            this.radRichTextEditor1.Size = new System.Drawing.Size(516, 349);
+            this.radRichTextEditor1.TabIndex = 13;
+            this.radRichTextEditor1.ThemeName = "Fluent";
+            this.radRichTextEditor1.TextChanged += new System.EventHandler(this.radRichTextEditor1_TextChanged);
+            // 
+            // radButton1
+            // 
+            this.radButton1.BackColor = System.Drawing.Color.LightBlue;
+            this.radButton1.Font = new System.Drawing.Font("Segoe UI", 14.25F);
+            this.radButton1.Location = new System.Drawing.Point(539, 252);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(147, 45);
+            this.radButton1.TabIndex = 14;
+            this.radButton1.Text = "Очистить форму";
+            this.radButton1.ThemeName = "Fluent";
+            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+            // 
+            // radButton3
+            // 
+            this.radButton3.BackColor = System.Drawing.Color.LightBlue;
+            this.radButton3.Font = new System.Drawing.Font("Segoe UI", 14.25F);
+            this.radButton3.Location = new System.Drawing.Point(539, 69);
+            this.radButton3.Name = "radButton3";
+            this.radButton3.Size = new System.Drawing.Size(147, 45);
+            this.radButton3.TabIndex = 15;
+            this.radButton3.Text = "Открыть папку с файлами";
+            this.radButton3.ThemeName = "Fluent";
+            this.radButton3.Click += new System.EventHandler(this.radButton3_Click);
+            // 
+            // radLabel1
+            // 
+            this.radLabel1.Font = new System.Drawing.Font("Segoe UI", 12.25F);
+            this.radLabel1.Location = new System.Drawing.Point(12, 376);
+            this.radLabel1.Name = "radLabel1";
+            this.radLabel1.Size = new System.Drawing.Size(209, 26);
+            this.radLabel1.TabIndex = 16;
+            this.radLabel1.Text = "Путь сохранения файлов: ";
+            // 
+            // radButton4
+            // 
+            this.radButton4.BackColor = System.Drawing.Color.LightBlue;
+            this.radButton4.Font = new System.Drawing.Font("Segoe UI", 14.25F);
+            this.radButton4.Location = new System.Drawing.Point(539, 383);
+            this.radButton4.Name = "radButton4";
+            this.radButton4.Size = new System.Drawing.Size(147, 26);
+            this.radButton4.TabIndex = 17;
+            this.radButton4.Text = "Изменить путь";
+            this.radButton4.ThemeName = "Fluent";
+            // 
+            // radLabel2
+            // 
+            this.radLabel2.Font = new System.Drawing.Font("Segoe UI", 14.25F);
+            this.radLabel2.Location = new System.Drawing.Point(548, 169);
+            this.radLabel2.Name = "radLabel2";
+            this.radLabel2.Size = new System.Drawing.Size(138, 30);
+            this.radLabel2.TabIndex = 18;
+            this.radLabel2.Text = "                        ";
+            // 
+            // radButton5
+            // 
+            this.radButton5.BackColor = System.Drawing.Color.LightBlue;
+            this.radButton5.Font = new System.Drawing.Font("Segoe UI", 14.25F);
+            this.radButton5.Location = new System.Drawing.Point(539, 312);
+            this.radButton5.Name = "radButton5";
+            this.radButton5.Size = new System.Drawing.Size(147, 45);
+            this.radButton5.TabIndex = 19;
+            this.radButton5.Text = "Помощь";
+            this.radButton5.ThemeName = "Fluent";
+            this.radButton5.Click += new System.EventHandler(this.radButton5_Click);
+            // 
+            // RadForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 363);
+            this.Controls.Add(this.radButton5);
+            this.Controls.Add(this.radLabel2);
+            this.Controls.Add(this.radButton4);
+            this.Controls.Add(this.radLabel1);
+            this.Controls.Add(this.radButton3);
+            this.Controls.Add(this.radButton1);
+            this.Controls.Add(this.radRichTextEditor1);
+            this.Controls.Add(this.radButton2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RadForm1";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "    Уведомления о назначении ЕДВ v.1.0.4";
+            this.ThemeName = "Fluent";
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radRichTextEditor1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Telerik.WinControls.Themes.FluentTheme fluentTheme1;
+        private Telerik.WinControls.UI.RadButton radButton2;
+        private Telerik.WinControls.UI.RadRichTextEditor radRichTextEditor1;
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadButton radButton3;
+        private Telerik.WinControls.UI.RadLabel radLabel1;
+        private Telerik.WinControls.UI.RadButton radButton4;
+        private Telerik.WinControls.UI.RadLabel radLabel2;
+        private Telerik.WinControls.UI.RadButton radButton5;
+    }
+}
---
+++ TelerikWinFormsApp14/RadForm1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+using Telerik.WinControls.UI;
+using System.Data.OleDb;
+using System.Text.RegularExpressions;
+using Word = Microsoft.Office.Interop.Word;
+using System.IO;
+
+
+namespace TelerikWinFormsApp14
+{
+    public partial class RadForm1 : RadForm
+    {
+        Word._Application application;
+
+        Object missingObj = System.Reflection.Missing.Value;
+        Object trueObj = true;
+        Object falseObj = false;
+        string fullName = Path.Combine(Environment.ExpandEnvironmentVariables("%temp%"), "Template.doc");
+        DataSet ds = new DataSet();
+        List<string> ListOfSnils = new List<string>();
+
+        string FIO;
+        string fa;
+        string im;
+        string ot;
+        string birthdate;
+        string address;
+        string numresh;
+        string datresh;
+        string sroks;
+        string srokpo;
+        string category;
+        string snils;
+        string currentday;
+
+        public RadForm1()
+        {
+            RadMessageBox.SetThemeName("Fluent");
+            InitializeComponent();
+            try
+            {
+                string subPath = @"C:\Uved\"; // your code goes here
+
+                bool exists = System.IO.Directory.Exists(subPath);
+
+                if (!exists)
+                { System.IO.Directory.CreateDirectory(subPath); }
+                File.WriteAllText(fullName, NVP.Properties.Resources.Template, Encoding.Default) ;
+               
+                 
+            }
+            catch (Exception)
+            {
+              
+            }
+           
+        }
+
+            private void Replword(string stub, string text, Word.Document wordDoc)
+            {
+            try
+            {
+                var range = wordDoc.Content;
+                range.Find.ClearFormatting();
+                range.Find.Execute(FindText: stub, ReplaceWith: text);
+
+            }
+            catch (Exception)
+            {
+                RadMessageBox.Show("Ошибка доступа к шаблону. Обратитесь к разработчику");
+                throw;
+            }
+             
+            }
+            public void SelectIdFromDB2(int i)
+            {
+                ds.Clear();
+            
+
+                using (OleDbConnection connection = new OleDbConnection
+                        (
+                        "Provider=IBMDADB2; " +
+                        "UID = db2admin; " +
+                        "PWD = Iw9o5JBW; " +
+                        "DATABASE = ROS; " +
+                        "HOSTNAME = 10.55.0.184; " +
+                        "PORT = 50000; " +
+                        "PROTOCOL = TCPIP;"
+                        ))
+                {
+
+                    try
+                    {
+                        connection.Open();
+                        // if (connection.State == ConnectionState.Open)
+                        // { MessageBox.Show("1"); }
+                        
+                        // if (connection.State == ConnectionState.Open)
+                        //{ MessageBox.Show("1"); }
+
+
+                        OleDbDataAdapter adapter = new OleDbDataAdapter();
+                        OleDbCommand command;
+
+                        command = new OleDbCommand("select M.FA, M.IM, M.OT, M.RDAT, M.ADR_INDEX, M.ADRFAKT, p.SROKS, p.SROKPO, p.NRESH, p.dresh, L.T " +
+                        "from GSP.GSP p " +
+                        "LEFT JOIN PF.MAN M ON M.ID = P.ID " +
+                        "left join GSP.LGKAT L ON p.L1 = L.K " +
+                        "WHERE M.NPERS = ? " +
+                        "AND (p.DRESH = (SELECT MAX(p1.DRESH) FROM GSP.GSP p1 WHERE M.ID = p1.ID GROUP BY p1.ID) " +
+                        "OR p.DRESH is NULL)", connection);
+                        command.Parameters.Add("M.NPERS", OleDbType.VarChar).Value = ListOfSnils[i];
+                        adapter.SelectCommand = command;
+                        adapter.Fill(ds);
+
+                        fa = ds.Tables[0].Rows[0].Field<string>("FA");
+                        im = ds.Tables[0].Rows[0].Field<string>("IM");
+                        ot = ds.Tables[0].Rows[0].Field<string>("OT");
+                        FIO = fa + " " + im + " " + ot;
+                        currentday = DateTime.Now.Date.ToString("dd.MM.yyyy");
+                        snils = ListOfSnils[i];
+                        birthdate = ds.Tables[0].Rows[0].Field<DateTime>("RDAT").ToString("dd.MM.yyyy") + " г.р.";
+                        address = ds.Tables[0].Rows[0].Field<string>("ADRFAKT");
+                        numresh = ds.Tables[0].Rows[0].Field<string>("NRESH");
+
+                        if (ds.Tables[0].Rows[0].Field<DateTime?>("DRESH") == null) { datresh = "Дата решения отсутствует"; }
+                        else datresh = ds.Tables[0].Rows[0].Field<DateTime>("DRESH").ToString("dd.MM.yyyy");
+
+                        if (ds.Tables[0].Rows[0].Field<DateTime?>("SROKS") == null) { sroks = "Дата назначения отсутствует"; }
+                        else sroks = ds.Tables[0].Rows[0].Field<DateTime>("SROKS").ToString("dd.MM.yyyy");
+
+                        if (ds.Tables[0].Rows[0].Field<DateTime?>("SROKPO") == null) { srokpo = "бессрочно"; }
+                        else srokpo = ds.Tables[0].Rows[0].Field<DateTime>("SROKPO").ToString("dd.MM.yyyy");
+
+                        category = ds.Tables[0].Rows[0].Field<string>("T");
+
+                    
+                     //radRichTextEditor1.Text = FIO + "\n" + birthdate + " года рождения" + "\n" + snils + "\n" + address + "\n" + numresh + " " + datresh + "\n" + sroks + " " + srokpo + "\n" + category;
+                       // string ass = radRichTextEditor1.Text;
+                   
+                        connection.Dispose();
+                        connection.Close();
+                        // if (connection.State == ConnectionState.Closed)
+                        // { MessageBox.Show("2"); }
+                    }
+
+                    catch (Exception)
+                    {
+                        RadMessageBox.Show("Ошибка доступа к базе НВП. Проверьте подключение к локальной сети");
+                    }
+                    InsertData();
+            }
+            }
+
+        public void InsertData()
+            { 
+            application = new Word.Application();
+            var wordDoc = application.Documents.Open(fullName);
+                try
+                {
+                    Replword("{FIO}", FIO, wordDoc);
+                    Replword("{DATER}", birthdate, wordDoc);
+                    Replword("{ADDR}", address, wordDoc);
+                    Replword("{SNILS}", snils, wordDoc);
+                    Replword("{DATEC}", currentday, wordDoc);
+                    Replword("{NAME}", im, wordDoc);
+                    Replword("{OTCH}", ot, wordDoc);
+                    Replword("{NRESH}", numresh, wordDoc);
+                    Replword("{RESHDATE}", datresh, wordDoc);
+                    Replword("{SROKS}", sroks, wordDoc);
+                    Replword("{SROKPO}", srokpo, wordDoc);
+                    Replword("{KATEG}", category, wordDoc);
+                }
+                catch (Exception)
+                {
+                    RadMessageBox.Show("Ошибка вставки данных в шаблон");
+                    throw;
+                }
+
+                try
+                {
+                
+               
+                 wordDoc.SaveAs(@"C:\Uved\Уведомление о назначении ЕДВ " + fa + " " + im + " " + ot + ".doc"); 
+                    wordDoc.Close();
+                    application.Quit();
+
+                //MessageBox.Show("Готово");
+                ds.Clear();
+                }
+                catch (Exception)
+                {
+                    RadMessageBox.Show("Ошибка сохранения файла");
+                    throw;
+                }
+     
+            }
+
+            private static string ExctraxtIni(string s)
+            {
+                var inits = Regex.Match(s, @"(\w+)\s+(\w+)\s+(\w+)").Groups;
+                return string.Format("{0} {1}. {2}.", inits[1], inits[2].Value[0], inits[3].Value[0]);
+            }
+
+        private void radButton2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ass = radRichTextEditor1.Text;
+                ass = ass.Replace(System.Environment.NewLine, string.Empty);
+                if (string.IsNullOrEmpty(ass) || Regex.IsMatch(ass, @"[a-zA-Zа-яА-Я]"))
+                { RadMessageBox.Show("Данные отсутствуют или содержат некорректные символы"); }
+                else
+                {
+                    ListOfSnils = Enumerable.Range(0, ass.Length / 14).Select(i => ass.Substring(i * 14, 14)).ToList();
+                    radRichTextEditor1.Text = string.Empty;
+                    for (int i = 0; i < ListOfSnils.Count; i++)
+                    {
+                        SelectIdFromDB2(i);
+                        radRichTextEditor1.Text += FIO + " - уведомление создано" + "\r\n";
+                        radLabel2.Refresh();
+                        radLabel2.Text = Convert.ToString(i+1) + "/" + Convert.ToString(ListOfSnils.Count);
+                    }
+                    radRichTextEditor1.Text += "Работа окончена" + "\r\n";
+                }
+            }
+            catch (Exception)
+            {
+                RadMessageBox.Show("Данные некорректны");
+                application.Quit();
+                System.Runtime.InteropServices.Marshal.FinalReleaseComObject(application);
+                throw;
+            }
+            
+        }
+
+        private void radButton1_Click(object sender, EventArgs e)
+        {
+            radRichTextEditor1.Text = string.Empty;
+        }
+
+        private void radButton3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer", "C:\\Uved");
+        }
+
+        private void radRichTextEditor1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radButton5_Click(object sender, EventArgs e)
+        {
+            RadMessageBox.Show(" Разработчик: Осипов Сергей \r\n ОПФР по Курганской области 2021г. \r\n Техподдержка: тел. 11-79");
+        }
+    }
+}
